4b9260e00db69d6280c10d3a3210543bd8cdcf03
Gaston Ariel Perrone <Perrone.gaston.ariel@outlook.com>
2025-10-25 22:13:06 -0300

    Model about Client is created. Change the DER documentation.

--- Concession_Management_System/apps/cliente/models.py
+++ Concession_Management_System/apps/cliente/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=30)
+    dni = models.CharField(max_length=10)
+    telefono = models.CharField(max_length=15)
+    calle = models.CharField(max_length=30)
+    numero_calle = models.IntegerField(null=False)
+    casa = models.BooleanField()
+    edificio = models.BooleanField()
+    country_privado = models.BooleanField()
+    piso = models.IntegerField(null=True,blank=True)
+    numero_casa = models.CharField(max_length=3, null=True, blank=True)
