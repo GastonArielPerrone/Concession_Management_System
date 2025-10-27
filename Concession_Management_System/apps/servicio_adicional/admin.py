a2304f23a0189f27beba0f71296c14355f1a0459
Gaston Ariel Perrone <Perrone.gaston.ariel@outlook.com>
2025-10-27 13:16:52 -0300

    admin.py about servicio_adicional is added.

--- Concession_Management_System/apps/servicio_adicional/admin.py
+++ Concession_Management_System/apps/servicio_adicional/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from apps.servicio_adicional.models import Servicio_adicional
 # Register your models here.
+@admin.register(Servicio_adicional)
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ("nombre_servicio", "venta")
+    list_filter = ("nombre_servicio", "venta","precio_servicio")
+    search_fields = ("nombre_servicio", "venta","precio_servicio",)
